14c853719a93fc1f7900ab8944ad8b606fafbf5c
Tyler Komoroske <tjkomor@clemson.edu>
2015-09-10 15:12:02 -0600

    braille can be converted into correct arrays and split into chunks of six. need to write method to join arrays to find keys

--- lib/braille.rb
+++ lib/braille.rb
@@ -3,10 +3,64 @@ class Braille
   attr_reader :braille
   def initialize(braille)
     @braille = braille
-    @values = []
+    @first_third = []
+    @second_third = []
+    @last_third = []
   end
 
-  def convert_to_bits
-    @braille.join.gsub('.', '1')
+  def convert_to_array
+    @braille.chars
   end
+
+  def assemble_values
+    index = 0
+    until convert_to_array.size / 3 == @first_third.size
+      @first_third << convert_to_array[index..index + 1]
+      index += 2
+      @second_third << convert_to_array[index..index + 1]
+      index += 2
+      @last_third << convert_to_array[index..index + 1]
+      index += 2
+    end
+  end
+
+  def first_third
+    @first_third.flatten.compact
+  end
+
+  def second_third
+    @second_third.flatten.compact
+  end
+
+  def last_third
+    @last_third.flatten.compact
+  end
+
+  def split_first_third_into_six
+    new_array = []
+    first_third.each_slice(6) { |braille| new_array << braille }
+    new_array
+  end
+
+  def split_second_third_into_six
+    new_array = []
+    second_third.each_slice(6) { |braille| new_array << braille }
+    new_array
+  end
+
+  def split_last_third_into_six
+    new_array = []
+    last_third.each_slice(6) { |braille| new_array << braille }
+    new_array
+  end
+
+
+  def find_letter(value)
+    Letters.new.find_key(value)
+  end
+
+  def create_words
+
+  end
+
 end
--- test/braille_test.rb
+++ test/braille_test.rb
@@ -12,11 +12,59 @@ class BrailleTest < Minitest::Test
     assert_equal '.00.', braille.braille
   end
 
-  def test_it_can_join_braille_into_six_digit_key
-    braille = Braille.new(['.0', '0.', '.0'])
-    assert_equal '100110', braille.convert_to_bits
+  def test_it_can_turn_lines_of_bits_into_array
+    braille = Braille.new('00..')
+    assert_equal ['0', '0', '.', '.'], braille.convert_to_array
   end
 
+  def test_it_can_assemble_correct_array_of_braille_values_for_first_third
+    braille = Braille.new(".00.000.....000.0.")
+    braille.assemble_values
+    assert_equal [".", "0", "0", ".", "0", "0"], braille.first_third
+  end
 
+  def test_it_can_assemble_correct_array_of_braille_values_for_second_third
+    braille = Braille.new(".00.000.....000.0.")
+    braille.assemble_values
+    assert_equal ["0", ".", ".", ".", "0", "."], braille.second_third
+  end
+
+  def test_it_can_assemble_correct_array_of_braille_values_for_last_third
+    braille = Braille.new(".00.000.....000.0.")
+    braille.assemble_values
+    assert_equal ["0", "0", ".", ".", "0", "."], braille.last_third
+  end
+
+  def test_it_can_find_value_of_one_letter
+    braille = Braille.new(".00.000.....000.0.")
+    braille.assemble_values
+    assert_equal 'm', braille.find_letter('001101')
+    assert_equal 't', braille.find_letter('100001')
+  end
+
+  def test_it_can_split_first_array_into_chunks_of_six
+    braille = Braille.new("0.0.00..0..000.0.0..000.....00......")
+    braille.assemble_values
+    assert_equal [["0", ".", ".", ".", "0", "0"], [".", ".", ".", ".", ".", "."]], braille.split_first_third_into_six
+  end
+
+  def test_it_can_split_second_array_into_chunks_of_six
+    braille = Braille.new("0.0.00..0..000.0.0..000.....00......")
+    braille.assemble_values
+    assert_equal [["0", ".", "0", ".", ".", "0"], ["0", "0", ".", ".", ".", "."]], braille.split_second_third_into_six
+  end
+
+  def test_it_can_split_third_array_into_chunks_of_six
+    braille = Braille.new("0.0.00..0..000.0.0..000.....00......")
+    braille.assemble_values
+    assert_equal [["0", "0", ".", "0", ".", "0"], ["0", ".", "0", "0", ".", "."]], braille.split_last_third_into_six
+  end
+
+  def test_it_can_find_values_of_multiple_letters
+    skip
+    braille = Braille.new("0.0.00..0..000.0.0..000.....00......")
+    braille.assemble_values
+    assert_equal "hey hi", braille.create_words
+  end
 
 end
